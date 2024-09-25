b086b6604301a856bdd40127f560c96c4dab2e71
Xxh <harbinxxh@gmail.com>
2024-09-25 08:42:23 +0800

    03_AssetActionUtility

--- Plugins/SuperManager/Source/SuperManager/SuperManager.Build.cs
+++ Plugins/SuperManager/Source/SuperManager/SuperManager.Build.cs
@@ -25,7 +25,7 @@ public class SuperManager : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
+				"Core","Blutility"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
