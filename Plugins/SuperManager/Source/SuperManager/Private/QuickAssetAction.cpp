f436a3e83ef56cc860f4f2a8f7836d0ba01b859f
Xxh <harbinxxh@gmail.com>
2024-09-25 17:30:12 +0800

    05_AssetBatchDuplication

--- Plugins/SuperManager/Source/SuperManager/Private/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/QuickAssetAction.cpp
@@ -3,14 +3,38 @@
 
 #include "QuickAssetAction.h"
 #include "DebugHeader.h"
+#include "EditorUtilityLibrary.h"
+#include "EditorAssetLibrary.h"
 
-void UQuickAssetAction::TestFunc()
+void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 {
-	//if (GEngine)
-	//{
-	//	GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Cyan, TEXT("Working"));
-	//}
+	if (NumOfDuplicates <= 0)
+	{
+		Print(TEXT("Please enter a VALID number"), FColor::Cyan);
+		return;
+	}
 
-	Print(TEXT("Working"), FColor::Cyan);
-	PrintLog(TEXT("Working"));
+	TArray<FAssetData> SelectedAssetsData = UEditorUtilityLibrary::GetSelectedAssetData();
+	uint32 Counter = 0;
+
+	for (const FAssetData& SelectedAssetData: SelectedAssetsData)
+	{
+		for (int32 i = 0; i < NumOfDuplicates; i++)
+		{
+			const FString SourceAssetPath = SelectedAssetData.ObjectPath.ToString();
+			const FString NewDuplicatedAssetName = SelectedAssetData.AssetName.ToString() + TEXT("_") + FString::FromInt(i + 1);
+			const FString NewPathName = FPaths::Combine(SelectedAssetData.PackagePath.ToString(), NewDuplicatedAssetName);
+
+			if (UEditorAssetLibrary::DuplicateAsset(SourceAssetPath, NewPathName))
+			{
+				UEditorAssetLibrary::SaveAsset(NewPathName, false);
+				++Counter;
+			}
+		}
+	}
+
+	if (Counter > 0)
+	{
+		Print(TEXT("Successfully Duplicated " + FString::FromInt(Counter) + " files"), FColor::Green);
+	}
 }
--- Plugins/SuperManager/Source/SuperManager/Public/DebugHeader.h
+++ Plugins/SuperManager/Source/SuperManager/Public/DebugHeader.h
@@ -3,6 +3,7 @@
 /*
 	说明：到目前为止，代码可以很好的编译
 	但稍后，当我们把 DebugHeader.h 文件包含在其他的文件中时，当我们编译会得到一些链接错误。
+	但不用担心，我们遇到它时在处理。
 */
 
 void Print(const FString& Message, const FColor& Color)
