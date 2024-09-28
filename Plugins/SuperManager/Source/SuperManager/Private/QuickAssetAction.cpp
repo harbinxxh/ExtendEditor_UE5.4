30590eec7fb159ffcd95c94aeb8821151640a188
Xxh <harbinxxh@gmail.com>
2024-09-28 09:05:05 +0800

    07_通过UEC++代码为复制的资产自动添加前缀

--- Plugins/SuperManager/Source/SuperManager/Private/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/QuickAssetAction.cpp
@@ -40,3 +40,41 @@ void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 		ShowNotifyInfo(TEXT("Successfully Duplicated " + FString::FromInt(Counter) + " files"));
 	}
 }
+
+void UQuickAssetAction::AddPrefixes()
+{
+	TArray<UObject*> SelectedObjects = UEditorUtilityLibrary::GetSelectedAssets();
+	uint32 Counter = 0;
+
+	for (UObject* SelectedObject:SelectedObjects)
+	{
+		if (!SelectedObject) continue;
+		
+		FString* PrefixFound = PrefixMap.Find(SelectedObject->GetClass());
+
+		if (!PrefixFound || PrefixFound->IsEmpty())
+		{
+			Print(TEXT("Failed to find prefix for class ") + SelectedObject->GetClass()->GetName(), FColor::Red);
+			continue;
+		}
+
+		FString OldName = SelectedObject->GetName();
+
+		if (OldName.StartsWith(*PrefixFound))
+		{
+			Print(TEXT("Already has prefix added "), FColor::Red);
+			continue;
+		}
+
+		const FString NewNameWithPrefix = *PrefixFound + OldName;
+		
+		UEditorUtilityLibrary::RenameAsset(SelectedObject, NewNameWithPrefix);
+
+		++Counter;
+	}
+
+	if (Counter > 0)
+	{
+		ShowNotifyInfo(TEXT("Successfully Rename ") + FString::FromInt(Counter) + " assets");
+	}
+}
