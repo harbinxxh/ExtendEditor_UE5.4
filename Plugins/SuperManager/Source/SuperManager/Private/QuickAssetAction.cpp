a08362f50e9b0024ba3ac054cabcd697f13988f6
Xxh <harbinxxh@gmail.com>
2024-10-03 10:58:48 +0800

    10_通过UEC++代码修复资产引用重定向

--- Plugins/SuperManager/Source/SuperManager/Private/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/QuickAssetAction.cpp
@@ -6,6 +6,8 @@
 #include "EditorUtilityLibrary.h"
 #include "EditorAssetLibrary.h"
 #include "ObjectTools.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "AssetToolsModule.h"
 
 void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 {
@@ -92,6 +94,8 @@ void UQuickAssetAction::RemoveUnusedAssets()
 	TArray<FAssetData> SelectedAssetsData = UEditorUtilityLibrary::GetSelectedAssetData();
 	TArray<FAssetData> UnsedAssetsData;
 
+	FixUpRedirectors();
+
 	for (const FAssetData& SelectedAssetData : SelectedAssetsData)
 	{
 		TArray<FString> AssetRefrencers = UEditorAssetLibrary::FindPackageReferencersForAsset(SelectedAssetData.GetSoftObjectPath().ToString());
@@ -113,4 +117,42 @@ void UQuickAssetAction::RemoveUnusedAssets()
 	if (NumOfAssetsDeleted == 0) return;
 	
 	ShowNotifyInfo(TEXT("Successfully deleted " + FString::FromInt(NumOfAssetsDeleted) + TEXT(" unused assets")));
+}
+
+void UQuickAssetAction::FixUpRedirectors()
+{
+	//创建重定向对象数组
+	TArray<UObjectRedirector*> RedirectorsToFixArray;
+
+	//需要使用模块管理器中 FModuleManager::LoadModuleChecked<>() 函数，动态加载 AssetRegistry 模块
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+
+	//设置过滤器
+	FARFilter Filter;
+	Filter.bRecursiveClasses = true; //将递归路径设置为 true，这将允许进入所有子文件夹。
+	Filter.PackagePaths.Emplace("/Game");//过滤器过滤的文件夹路径
+	
+	//Filter.ClassNames.Emplace("ObjectRedirector");//
+	Filter.ClassPaths.Emplace("ObjectRedirector");//需要指定要过滤的类的名称
+	/* 
+	* warning C4996 : 'FARFilter::ClassNames' : Class names are now represented by path names.Please use ClassPaths.
+	* Please update your code to the new API before upgrading to the next release, otherwise your project will no longer compile.
+	*/
+
+	//将包含通过过滤器所筛选出来的资产数据(FAssetData)
+	TArray<FAssetData> OutRedirectors;
+	AssetRegistryModule.Get().GetAssets(Filter, OutRedirectors);
+
+	for (const FAssetData& RedirectorData : OutRedirectors)
+	{
+		//需要将 FAssetData 数据格式，转换成 UObjectRedirector 数据格式
+		if (UObjectRedirector* RedirectorToFix = Cast<UObjectRedirector>(RedirectorData.GetAsset()))
+		{
+			RedirectorsToFixArray.Add(RedirectorToFix);
+		}
+	}
+
+	//需要调用【AssetToolsModule】模块中的【FixUpRefrencers()】函数，这是用来修复重定向器
+	FAssetToolsModule& AssetToolsModule = FModuleManager::LoadModuleChecked<FAssetToolsModule>(TEXT("AssetTools"));
+	AssetToolsModule.Get().FixupReferencers(RedirectorsToFixArray);
 }
